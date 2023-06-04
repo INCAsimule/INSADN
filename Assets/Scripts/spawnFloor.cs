f2246c320695db2ea1d01cee362685c406aa793b
XerusN <xavier.noel24@gmail.com>
2023-06-04 19:20:54 +0200

    new floorSpawn

--- Assets/Scripts/spawnFloor.cs
+++ Assets/Scripts/spawnFloor.cs
@@ -7,24 +7,14 @@ public class spawnFloor : MonoBehaviour
     public int radius;     //will actually do radius + 0.5
     public GameObject tilePrefab;
     private Vector3 position;
-    private Vector3 rotation;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = -radius; i < radius + 1; i++)
-        {
-            for (int j = -radius; j < radius + 1; j++)
-            {
-                if (i*i + j*j <= radius*radius)
-                {
-                    position = new Vector3(i - 0.5f, -tilePrefab.transform.localScale.y, j + 0.5f);
-                    Instantiate(tilePrefab, position, new Quaternion(0, 0, 0, 0));
-                }
-            }
-        }
-
+        position = new Vector3(0, -tilePrefab.transform.localScale.y, 0);
+        tilePrefab.transform.localScale = new Vector3(radius, tilePrefab.transform.localScale.y, radius);
+        Instantiate(tilePrefab, position, new Quaternion(0,0,0,0));
     }
 
     // Update is called once per frame
